8ef20df3312ada4afebd0504ba1931e8521275a1
Francois Jeannotte <francois.jeannotte@gmail.com>
2013-09-04 21:57:28 -0500

    Major changes to config file, actual code generation to be reworked.

--- ckitgen.py
+++ ckitgen.py
@@ -1,16 +1,44 @@
 import collections
 import itertools
-import json
 import os.path
+from pprint import pprint
 import re
 import sys
 
-# Load the config file (required)
-if not os.path.isfile('ckit.config'):
-    print 'ckit.config file is missing, please create it.'
-    sys.exit(1)
-with open('ckit.config', 'r') as f:
-    config = json.load(f)
+def process_config_blocks(s):
+    if s.find('{') < 0:
+        return s
+    d     = dict()
+    name  = ''
+    block = ''
+    depth = 0
+    for c in s:
+        if c == '{':
+            if depth > 0:
+                block += c
+            depth += 1
+        elif c == '}':
+            depth -= 1
+            if depth == 0:
+                d[name.strip()] = process_config_blocks(block.strip())
+                name = ''
+                block = ''
+            else:
+                block += c
+        else:
+            if depth == 0:
+                name += c
+            else:
+                block += c
+    return d
+
+def load_config():
+    if not os.path.isfile('ckit.config'):
+        print 'ckit.config file is missing, please create it.'
+        sys.exit(1)
+    with open('ckit.config', 'r') as f:
+        return process_config_blocks(f.read())
+config = load_config()
 
 # The list of standard include files that we will need
 std_incs = 'assert math stdio stdlib string'.split()
@@ -32,8 +60,6 @@ with open('ckit.h', 'w') as outf_h, open('ckit.c', 'w') as outf_c:
 
     # Write initial stuff to both files
     outf_h.write('#ifndef CKIT_H\n#define CKIT_H\n\n')
-    for inc in config['includes']:
-        outf_h.write('#include "{}"\n'.format(inc))
     outf_c.write('#include "ckit.h"\n\n')
     for inc in std_incs:
         outf_c.write('#include <{}.h>\n'.format(inc))
