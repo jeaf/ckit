77c3a30d0d13365d9e235db5307057881caf975f
Francois Jeannotte <francois.jeannotte@gmail.com>
2013-09-20 17:32:42 -0500

    Improve asserts in deque_circbuf.

--- deque_circbuf.c
+++ deque_circbuf.c
@@ -1,3 +1,16 @@
+#define $name_ASSERT_BASE                      \
+    assert(d);                                 \
+    assert(d->dbgstate == CONSTRUCTED);        
+
+#define $name_ASSERT_NOTEMPTY                  \
+    assert(d->buf);                            \
+    assert(d->size     >  0);                  \
+    assert(d->size     <= d->capacity);        \
+    assert(d->front    <  d->capacity);        \
+    assert(d->back     <  d->capacity);        \
+    assert(d->size < 2 || d->front != d->back);\
+    assert(d->size > 1 || d->front == d->back);
+
 void $name_ctor($name* d)
 {
     assert(d);
@@ -9,7 +22,8 @@ void $name_ctor($name* d)
 
 void $name_dtor($name* d)
 {
-    assert(d);
+    $name_ASSERT_BASE
+
     free(d->buf);
     #ifndef NDEBUG
         d->dbgstate = DESTRUCTED;
@@ -18,8 +32,8 @@ void $name_dtor($name* d)
 
 $type* $name_pushback($name* d)
 {
-    assert(d);
-    assert(d->dbgstate == CONSTRUCTED);
+    $name_ASSERT_BASE
+
     if (d->size == d->capacity)
     {
         if (d->capacity == 0)
@@ -32,31 +46,32 @@ $type* $name_pushback($name* d)
             d->capacity *= 2;
             d->buf      = realloc(d->buf, sizeof($type) * d->capacity);
             assert(d->buf);
+            assert(!"incomplete, must split the two parts");
         }
     }
     assert(d->capacity > d->size);
     d->back = (d->back + 1) % d->capacity;
     ++d->size;
-    assert(d->back < d->capacity);
+    
+    $name_ASSERT_NOTEMPTY
+
     $ctor{d->buf[d->back]}
     return &d->buf[d->back];
 }
 
 $type* $name_front($name* d)
 {
-    assert(d);
-    assert(d->dbgstate == CONSTRUCTED);
-    assert(d->size > 0);
-    assert(d->front < d->capacity);
+    $name_ASSERT_BASE
+    $name_ASSERT_NOTEMPTY
+
     return &d->buf[d->front];
 }
 
 void $name_popfront($name* d)
 {
-    assert(d);
-    assert(d->dbgstate == CONSTRUCTED);
-    assert(d->size > 0);
-    assert(d->front < d->capacity);
+    $name_ASSERT_BASE
+    $name_ASSERT_NOTEMPTY
+
     $dtor{d->buf[d->front]}
     --d->size;
     d->front = (d->front + 1) % d->capacity;
